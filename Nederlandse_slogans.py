8c744dc412b40430839f31e87727103926f08adc
Jesse Kuipers <jessekuipers95@gmail.com>
2023-10-05 23:57:11 +0200

    Update Nederlandse_slogans.py

--- Nederlandse_slogans.py
+++ Nederlandse_slogans.py
@@ -15,6 +15,11 @@ from langchain.prompts.chat import (
 
 from langchain.chains import LLMChain
 
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = "https://api.langchain.plus"
+os.environ["LANGCHAIN_API_KEY"] = os.environ.get("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_PROJECT"] = "solgan-generator"
+
 apikey = os.getenv('OPENAI_API_KEY')
 
 st.sidebar.success('Select a page above!')
